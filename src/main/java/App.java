4b8dfb659e63eef0e02edf3346d05d7ee3ac7bd7
Charisma <actuallycharisma@outlook.com>
2024-06-06 12:26:44 +0100

    resolve merge conflict

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,7 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Added this on line 5");
+
+        System.out.println("Added this to line 7 on the main branch ");
     }
 }
