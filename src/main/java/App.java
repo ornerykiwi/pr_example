899b78a53d0d78449f2d450913b95701183f02ae
Charisma <actuallycharisma@outlook.com>
2024-06-06 12:37:11 +0100

    checking out this feature c

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,7 @@ public class App {
         System.out.println("Added this on line 5");
 
         System.out.println("Added this to line 7 on the main branch ");
+
+        System.out.println("Hi I'm checking out feature C");
     }
 }
